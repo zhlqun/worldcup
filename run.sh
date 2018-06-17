b07fc844b6cb3bfaa4cec77a43c271c0552a3f0d
Fish <Fish@Fish-3.local>
2018-06-17 12:07:40 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #encoding=utf8
-
+ps -ef|grep "server.py" | grep -v grep|cut -c 9-15|xargs kill -9
+ps -ef|grep "parse_page.py" | grep -v grep|cut -c 9-15|xargs kill -9
 cd sina_dynamic/
 nohup python parse_page.py &
 nohup python server.py > log 2>&1 &
